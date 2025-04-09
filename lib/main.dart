bae10322e8a4bbe26486494927971fa2ace43950
lunajulio <lunajulio@uninorte.edu.co>
2025-04-08 20:13:44 -0500

    integration test

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'pages/homepage.dart';
 import 'controllers/navigation_controller.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'routes/app_routes.dart';
-import 'package:intl/intl.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();  
@@ -28,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         scaffoldBackgroundColor: Colors.white,
-      ), // Faltaba este cierre de paréntesis
+      ),
       home: HomePage(),
       getPages: AppRoutes.pages,
     );
